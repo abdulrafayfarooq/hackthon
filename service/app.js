bb8ebe6d25e016896297822d51b84718b9df0500
rafay1234321 <abdulrafaymalik16@gmail.com>
2025-01-25 19:53:10 +0500

    build

--- service/app.js
+++ service/app.js
@@ -1,32 +1,28 @@
+require('dotenv').config(); // Load environment variables
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const path = require('path');
 const cors = require('cors');
-require('dotenv').config();
 
+// Initialize Express app
 const app = express();
+const port = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
-app.use(morgan('dev'));
-app.use(bodyParser.json());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://localhost:3001', "*"], 
+  credentials: true
+}));
+app.use(express.json());
 
-// MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB connection error:', err));
-
-// Routes
-const apiRoutes = require('./routes/api');
-app.use('/api', apiRoutes);
+// Serve static files from the 'web/build' folder
+app.use('/', express.static(path.join(__dirname, '../client/build')));
+app.use('*', express.static(path.join(__dirname, '../client/build')));
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+startServer();
